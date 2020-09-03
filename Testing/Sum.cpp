203e20fdb10a8a3e18a4b5d62e142045b3ae9d19
SirHellcat <framadorv@unal.edu.co>
2020-09-03 23:02:56 +0000

    Program for adding integers

---
+++ Testing/Sum.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main(){
+  int a; int b;
+
+  std::cout << "Enter number a \n";
+
+  std:: cin >> a;
+
+  std::cout << "Enter number b \n";
+
+  std:: cin >> b;
+
+  std::cout << "a + b = " << a+b << '\n';
+
+  return 0;
+
+}
