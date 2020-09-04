656b31ea5d276026c8bcea05a59c663664e2c166
SirHellcat <framadorv@unal.edu.co>
2020-09-04 02:34:16 +0000

    Ejercicio1 para calcular la precisión numérica de una función

---
+++ NumericalErr/Ejercicio1.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <cstdlib>
+#include <math.h>
+#include <iomanip>
+
+float ff(float x) {
+  return 5 - sqrt (25 + pow(x,2));
+}
+
+float ffmejor(float x) {
+  return 5 - sqrt (25 + x * x);
+}
+
+int main(int argc, char *argv[]){
+
+  int x=0;
+  float paso = std::atof(argv[1]); //Para poder modificar el valor de cada paso
+
+  int P = std::atoi(argv[2]); //Para poder modificar la presición
+
+  //La parte de la notación científica
+  std::cout.setf(std::ios::scientific);
+
+  //En vez de usar un contador igual al paso ingresado (0.01), se mantiene el contador igual a uno para evitar errores
+  while(x <= (1.0 / paso)){
+
+  std::cout << std::setprecision(1) << x * paso << "\t"
+            << std::setprecision(P) << ff(x * paso) << "\t"
+            << std::setprecision(P) << ffmejor(x * paso)
+            << "\n";
+  x = x + 1;
+  }
+
+  return 0;
+
+}
